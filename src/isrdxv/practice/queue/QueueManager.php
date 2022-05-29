921883150f8eb07782d7d6b226f3dc7adafd637c
srclauyt@gmail.com <srclauyt@gmail.com>
2022-05-29 14:29:58 -0300

    Prepare system from Queue, for the arenas

--- src/isrdxv/practice/queue/QueueManager.php
+++ src/isrdxv/practice/queue/QueueManager.php
@@ -1,3 +1,35 @@
 <?php
-
+//TODO: Terminar el sistema XD
 namespace isrdxv\practice\queue;
+
+use pocketmine\utils\SingletonTrait;
+
+use isrdxv\practice\queue\Queue;
+
+class QueueManager
+{
+  use SingletonTrait;
+  
+  private array $queues = [];
+  
+  public function create(): void;
+  
+  public function delete(): void;
+  
+  public function getQueueById(): ?Queue;
+  
+  public function getQueueByName(): ?Queue;
+  
+  public function getQueues(): array;
+  
+  public function getRandomInt(): int
+  {
+    return random_int(0, 999);
+  }
+  
+  public function getRandomString(): string
+  {
+    return bin2hex(random_bytes(3));
+  }
+  
+}
