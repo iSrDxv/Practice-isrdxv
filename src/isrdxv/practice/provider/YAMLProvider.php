798d61f24e7cc48a7bd89d074998bfb59a257e54
srclauyt@gmail.com <srclauyt@gmail.com>
2022-06-15 11:11:03 -0300

    add default leaderboards

--- src/isrdxv/practice/provider/YAMLProvider.php
+++ src/isrdxv/practice/provider/YAMLProvider.php
@@ -23,11 +23,14 @@ class YAMLProvider
     if (!is_dir($folder = Loader::getInstance()->getDataFolder() . "bans" . DIRECTORY_SEPARATOR)) {
       @mkdir($folder);
     }
+    if (!is_dir($folder = Loader::getInstance()->getDataFolder() . "leaderboard" . DIRECTORY_SEPARATOR)) {
+      @mkdir($folder);
+    }
   }
   
   public function saveDataSession(string $name, array $data): void 
   {
-    if ($data === [] || $name === "") return;
+    if (empty($data) || empty($name)) return;
     $config = new Config(Loader::getInstance()->getDataFolder() . "players" . DIRECTORY_SEPARATOR . $name . ".yml", Config::YAML);
     $config->setAll($data);
     $config->save();
@@ -89,13 +92,32 @@ class YAMLProvider
     return (new Config(Loader::getInstance()->getDataFolder() . "players" . DIRECTORY_SEPARATOR . $name . ".yml", Config::YAML))->getNested("points");
   }
   
-  /*public function isPartner(string $username): bool
+  public function getDeathsFile(): Config
   {
-    $config = new Config(Loader::getInstance()->getDataFolder() . "players" . DIRECTORY_SEPARATOR . $username . ".yml", Config::YAML);
-    if ($config->get("associate") !== false or $config->exists("associate")) {
-      return true;
-    }
-    return false;
-  }*/
+    return new Config(Loader::getInstance()->getDataFolder() . "leaderboard" . DIRECTORY_SEPARATOR . "deaths.json", Config::JSON);
+  }
+  
+  public function getMurdersFile(): Config
+  {
+    return new Config(Loader::getInstance()->getDataFolder() . "leaderboard" . DIRECTORY_SEPARATOR . "murders.json", Config::JSON);
+  }
+  
+  public function getKDRFile(): Config
+  {
+    return new Config(Loader::getInstance()->getDataFolder() . "leaderboard" . DIRECTORY_SEPARATOR . "kdr.json", Config::JSON);
+  }
+  
+  public function setDefaultLeaderboards(string $name): void
+  {
+    $deaths = $this->getDeathsFile();
+    $murders = $this->getMurdersFile();
+    $kdr = $this->getKDRFile();
+    $deaths->set($name, 0);
+    $murders->set($name, 0);
+    $kdr->set($name, 0.0);
+    $deaths->save();
+    $murders->save();
+    $kdr->save();
+  }
   
 }
