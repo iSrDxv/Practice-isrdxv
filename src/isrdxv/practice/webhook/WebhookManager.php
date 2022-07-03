b36ddd87aaeb5ccd5a87e9c2cfa288504a6849bf
srclauyt@gmail.com <srclauyt@gmail.com>
2022-07-03 10:59:41 -0300

    prepare discord webhook (PD: the webhook is from a server with 1 user)

--- src/isrdxv/practice/webhook/WebhookManager.php
+++ src/isrdxv/practice/webhook/WebhookManager.php
@@ -2,20 +2,27 @@
 
 namespace isrdxv\practice\webhook;
 
-use libs\discord\Webhook;
+use libs\discord\{
+  Webhook,
+  Message,
+  Embed
+};
 
 use isrdxv\practice\Loader;
 
 class WebhookManager
 {
+  private Loader $loader;
+  
   private Webhook $user;
   
   private Webhook $staff;
   
   public function __construct(Loader $loader)
   {
-    $this->user = new Webhook($loader->getConfig()->get("discord-logs")["user"]["url"]);
-    $this->staff = new Webhook($loader->getConfig()->get("discord-logs")["staff"]["url"]);
+    $this->loader = $loader;
+    $this->user = new Webhook($loader->getConfig()->get("discord-logs")["user"]);
+    $this->staff = new Webhook($loader->getConfig()->get("discord-logs")["staff"]);
   }
   
   public function getWebhookUser(): Webhook
@@ -26,6 +33,30 @@ class WebhookManager
   public function getWebhookStaff(): Webhook
   {
     return $this->staff;
+  }
+  
+  public function sendMessage(string $type, string $content = ""): void
+  {
+    if ($type === "user") {
+      $msg = new Message();
+      $msg->setUsername($this->loader->getConfig()->get("discord")["username"]);
+      $msg->setAvatarURL($this->loader->getConfig()->get("discord")["avatarURL"]);
+      $msg->setContent($content);
+      $this->user->send($msg);
+    } else {
+      $msg = new Message();
+      $msg->setUsername($this->loader->getConfig()->get("discord")["username"]);
+      $msg->setAvatarURL($this->loader->getConfig()->get("discord")["avatarURL"]);
+      $msg->setContent($content);
+      $this->staff->send($msg);
+    }
+  }
+  
+  public function sendEmbed(string $type): void
+  {
+    if ($type === "user") {
+      
+    }
   }
   
 }
