598206b0b20989848524a306fa5344a2dfca8a09
srclauyt@gmail.com <srclauyt@gmail.com>
2022-07-06 22:58:42 -0300

    order the code

--- src/isrdxv/practice/game/GameTask.php
+++ src/isrdxv/practice/game/GameTask.php
@@ -29,19 +29,18 @@ class GameTask extends Task
           $game->setPhase($this->loader->getGameManager()::PHASE_STARTING);
         }
       }elseif ($game->getPhase() === $this->loader->getGameManager()::PHASE_STARTING) {
-          $this->time--;
-          if ($this->time <= 0) {
-            $game->sendAction(function(Session $session) use($game): void {
-              //foreach($game->getPlayers() as $player) {
-                $session->setScoreboard(new GameScoreboard($sesion, $player, $game));
-              }
-            );
-            $game->setPhase($this->loader->getGameManager()::PHASE_PLAYING);
-          } else {
-            $game->sendAction(function(Session $session) {
-              $session->sendMessage(new TranslationMessage("game-starting", ["time" => $this->time]));
-            });
-          }
+        $this->time--;
+        if ($this->time <= 0) {
+          $game->sendAction(function(Session $session) use($game): void {
+            //foreach($game->getPlayers() as $player) {
+            $session->setScoreboard(new GameScoreboard($sesion, $player, $game));
+          });
+        $game->setPhase($this->loader->getGameManager()::PHASE_PLAYING);
+        } else {
+          $game->sendAction(function(Session $session) {
+            $session->sendMessage(new TranslationMessage("game-starting", ["time" => $this->time]));
+          });
+        }
       }elseif ($game->getPhase() === $this->loader->getGameManager()::PHASE_PLAYING) {
         $game->setTime();
       }elseif ($game->getPhase() === $this->loader->getGameManager()::PHASE_ENDING) {
