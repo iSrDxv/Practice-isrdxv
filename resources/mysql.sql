d2d726af356033a08b9cf2410180420bf8dfb06c
srclauyt@gmail.com <srclauyt@gmail.com>
2023-12-15 01:33:04 -0300

    This is more explanatory so when you change the name you do not lose your data and it is sent to the website

--- resources/mysql.sql
+++ resources/mysql.sql
@@ -1,19 +1,19 @@
 -- #! mysql
 
 -- #{ table.bans
-CREATE TABLE IF NOT EXISTS bans(name VARCHAR(30) NOT NULL UNIQUE, event VARCHAR(25), expired TEXT, reason TEXT, staff VARCHAR(30))
+CREATE TABLE IF NOT EXISTS bans(id INT UNIQUE AUTO_INCREMENT PRIMARY KEY, banned_user VARCHAR(30) NOT NULL, event VARCHAR(25), expired TEXT, reason TEXT, staff VARCHAR(30))
 -- #}
 
 -- #{ table.duration
-CREATE TABLE IF NOT EXISTS duration(xuid VARCHAR(18) NOT NULL UNIQUE, name VARCHAR(30), voted TEXT, donated TEXT, muted TEXT, lastplayed TEXT, totalonline TEXT, warnings, INT)
+CREATE TABLE IF NOT EXISTS duration(xuid VARCHAR(18) NOT NULL UNIQUE, voted TEXT, donated TEXT, muted TEXT, lastplayed TEXT, totalonline TEXT, warnings, INT)
 -- #}
 
 -- #{ table.stats
-CREATE TABLE IF NOT EXISTS stats(xuid VARCHAR(18) NOT NULL UNIQUE, name VARCHAR(30), kills INT, donated TEXT, deaths INT, coin INT)
+CREATE TABLE IF NOT EXISTS stats(xuid VARCHAR(18) NOT NULL UNIQUE, kills INT, donated TEXT, deaths INT, coin INT)
 -- #}
 
 -- #{ table.staff.stats
-CREATE TABLE IF NOT EXISTS staff_stats(xuid VARCHAR(18) NOT NULL UNIQUE, name VARCHAR(30), kicks INT, silenceds INT, reports INT)
+CREATE TABLE IF NOT EXISTS staff_stats(xuid VARCHAR(18) NOT NULL UNIQUE, kicks INT, silenceds INT, reports INT)
 -- #}
 
 -- #{ table.kits
