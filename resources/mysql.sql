f538ace25e0e4f8b4f5683daaadd0e05bad0761a
srclauyt@gmail.com <srclauyt@gmail.com>
2023-12-15 01:34:35 -0300

    We need the name to display on the web xd (and for the api)

--- resources/mysql.sql
+++ resources/mysql.sql
@@ -5,17 +5,17 @@ CREATE TABLE IF NOT EXISTS bans(id INT UNIQUE AUTO_INCREMENT PRIMARY KEY, banned
 -- #}
 
 -- #{ table.duration
-CREATE TABLE IF NOT EXISTS duration(xuid VARCHAR(18) NOT NULL UNIQUE, voted TEXT, donated TEXT, muted TEXT, lastplayed TEXT, totalonline TEXT, warnings, INT)
+CREATE TABLE IF NOT EXISTS duration(xuid VARCHAR(18) NOT NULL UNIQUE, username VARCHAR(30) NOT NULL, voted TEXT, donated TEXT, muted TEXT, lastplayed TEXT, totalonline TEXT, warnings, INT)
 -- #}
 
 -- #{ table.stats
-CREATE TABLE IF NOT EXISTS stats(xuid VARCHAR(18) NOT NULL UNIQUE, kills INT, donated TEXT, deaths INT, coin INT)
+CREATE TABLE IF NOT EXISTS stats(xuid VARCHAR(18) NOT NULL UNIQUE, username VARCHAR(30) NOT NULL, kills INT, donated TEXT, deaths INT, coin INT)
 -- #}
 
 -- #{ table.staff.stats
-CREATE TABLE IF NOT EXISTS staff_stats(xuid VARCHAR(18) NOT NULL UNIQUE, kicks INT, silenceds INT, reports INT)
+CREATE TABLE IF NOT EXISTS staff_stats(xuid VARCHAR(18) NOT NULL UNIQUE, username VARCHAR(30) NOT NULL, kicks INT, silenceds INT, reports INT)
 -- #}
 
 -- #{ table.kits
-CREATE TABLE IF NOT EXISTS kits(xuid VARCHAR(18) NOT NULL UNIQUE, name VARCHAR(30))
+CREATE TABLE IF NOT EXISTS kits(xuid VARCHAR(18) NOT NULL UNIQUE, username VARCHAR(30) NOT NULL)
 -- #}
