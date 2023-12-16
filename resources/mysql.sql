f7d4fafd678e624cb9d74ba4c8425c41bef5d90a
srclauyt@gmail.com <srclauyt@gmail.com>
2023-12-16 01:31:42 -0300

    update mysql.sql

--- resources/mysql.sql
+++ resources/mysql.sql
@@ -5,15 +5,15 @@ CREATE TABLE IF NOT EXISTS bans(id INT AUTO_INCREMENT PRIMARY KEY, banned_user V
 -- #}
 
 -- #{ table.duration
-CREATE TABLE IF NOT EXISTS duration(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, username VARCHAR(30) NOT NULL, voted TEXT, donated TEXT, muted TEXT, lastplayed TEXT, totalonline TEXT, warnings, INT)
+CREATE TABLE IF NOT EXISTS duration(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, name VARCHAR(30) NOT NULL, voted TEXT, donated TEXT, muted TEXT, lastplayed TEXT, totalonline TEXT, warnings, INT)
 -- #}
 
 -- #{ table.stats
-CREATE TABLE IF NOT EXISTS stats(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, username VARCHAR(30) NOT NULL, kills INT, donated TEXT, deaths INT, coin INT)
+CREATE TABLE IF NOT EXISTS stats(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, name VARCHAR(30) NOT NULL, kills INT, donated TEXT, deaths INT, coin INT)
 -- #}
 
 -- #{ table.staff.stats
-CREATE TABLE IF NOT EXISTS staff_stats(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, username VARCHAR(30) NOT NULL, kicks INT, silenceds INT, reports INT)
+CREATE TABLE IF NOT EXISTS staff_stats(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, name VARCHAR(30) NOT NULL, kicks INT, silenceds INT, reports INT)
 -- #}
 
 -- #{ table.kits
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS kits(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, us
 -- #}
 
 -- #{ table.user_data
-CREATE TABLE IF NOT EXISTS user_data(xuid VARCHAR(18) NOT NULL UNIQUE, username VARCHAR(30) NOT NULL, custom_name VARCHAR(30) NULL, alias VARCHAR(25) NULL, skin LONGTEXT)
+CREATE TABLE IF NOT EXISTS user_data(xuid VARCHAR(18) NOT NULL UNIQUE, name VARCHAR(30) NOT NULL, custom_name VARCHAR(30) NULL, alias VARCHAR(25) NULL, skin LONGTEXT)
 -- #}
 
 -- #{ alter.table.bans
