4709f445849ce05e388e9f51ffb157fb3c4b37ad
srclauyt@gmail.com <srclauyt@gmail.com>
2023-12-25 15:25:52 -0300

    update mysql.sql

--- resources/mysql.sql
+++ resources/mysql.sql
@@ -4,7 +4,7 @@
 CREATE TABLE IF NOT EXISTS bans(id INT AUTO_INCREMENT PRIMARY KEY, banned_user VARCHAR(30) NOT NULL, event VARCHAR(25), expired TEXT, reason TEXT, staff VARCHAR(30))
 -- #}
 
--- #{ table.duration
+-- #{ query.declarartion
 CREATE TABLE IF NOT EXISTS duration(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, voted TEXT, donated TEXT, muted TEXT, lastplayed TEXT, totalonline TEXT, time_join_server TEXT, warnings INT)
 -- #}
 
@@ -22,7 +22,7 @@ INSERT INTO duration(xuid, voted, donated, muted, lastplayed, totalonline, warni
 -- #}
 -- #}
 
--- #{ table.murders
+-- #{ query.declarartion
 CREATE TABLE IF NOT EXISTS murders(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, combo INT, gapple INT, nodebuff INT, trapping INT, bridge INT, classic INT)
 -- #}
 
@@ -39,7 +39,7 @@ INSERT INTO murders(xuid, combo, gapple, nodebuff, trapping, bridge, classic) VA
 -- #}
 -- #}
 
--- #{ table.kills
+-- #{ query.declarartion
 CREATE TABLE IF NOT EXISTS kills(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, combo INT, gapple INT, nodebuff INT, trapping INT, bridge INT, classic INT)
 -- #}
 
@@ -56,7 +56,7 @@ INSERT INTO kills(xuid, combo, gapple, nodebuff, trapping, bridge, classic) VALU
 -- #}
 -- #}
 
--- #{ table.points
+-- #{ query.declarartion
 CREATE TABLE IF NOT EXISTS points(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, combo INT, gapple INT, nodebuff INT, trapping INT, bridge INT, classic INT)
 -- #}
 
@@ -73,11 +73,11 @@ INSERT INTO points(xuid, combo, gapple, nodebuff, trapping, bridge, classic) VAL
 -- #}
 -- #}
 
--- #{ table.staff.stats
+-- #{ query.declarartion
 CREATE TABLE IF NOT EXISTS staff_stats(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, name VARCHAR(30) NOT NULL, kicks INT, silenceds INT, reports INT)
 -- #}
 
--- #{ table.won_events
+-- #{ query.declarartion
 CREATE TABLE IF NOT EXISTS won_events(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, name VARCHAR(30) NOT NULL, title TEXT, description VARCHAR(100), prize VARCHAR(30))
 -- #}
 
@@ -92,11 +92,11 @@ INSERT INTO won_events(xuid, name, title, description, prize) VALUES (:xuid, :us
 -- #}
 -- #}
 
--- #{ table.kits
+-- #{ query.declarartion
 CREATE TABLE IF NOT EXISTS kits(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, username VARCHAR(30) NOT NULL)
 -- #}
 
--- #{ table.user_data
+-- #{ query.declarartion
 CREATE TABLE IF NOT EXISTS user_data(xuid VARCHAR(18) NOT NULL UNIQUE, name VARCHAR(30) NOT NULL, custom_name VARCHAR(30) NULL, alias VARCHAR(25) NULL, language TEXT, skin LONGTEXT, coin INT)
 -- #}
 
@@ -112,7 +112,7 @@ INSERT INTO user_data(xuid, name, custom_name, alias, language, skin, coin) VALU
 -- #}
 -- #}
 
--- #{ table.settings
+-- #{ query.declarartion
 CREATE TABLE IF NOT EXISTS settings(xuid VARCHAR(18) NOT NULL UNIQUE, scoreboard BOOLEAN, queue BOOLEAN, cps BOOLEAN, auto_join BOOLEAN)
 -- #}
 
