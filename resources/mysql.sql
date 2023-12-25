47410409b916bdc4a4e683f5f42cae140a09079f
srclauyt@gmail.com <srclauyt@gmail.com>
2023-12-25 15:01:47 -0300

    update mysql.sql

--- resources/mysql.sql
+++ resources/mysql.sql
@@ -10,11 +10,20 @@ CREATE TABLE IF NOT EXISTS bans(id INT AUTO_INCREMENT PRIMARY KEY, banned_user V
 --DURATION (of any shit)
 
 -- #{ table.duration
-CREATE TABLE IF NOT EXISTS duration(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, voted TEXT, donated TEXT, muted TEXT, lastplayed TEXT, totalonline TEXT, join_first_time_server TEXT, warnings INT)
+CREATE TABLE IF NOT EXISTS duration(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, voted TEXT, donated TEXT, muted TEXT, lastplayed TEXT, totalonline TEXT, time_join_server TEXT, warnings INT)
 -- #}
 
--- #{ table.duration.insert
-INSERT INTO duration(xuid, voted, donated, muted, lastplayed, totalonline, warnings) VALUES (:xuid, :voted, :donated, :muted, :lastplayed, :totalonline, :join_first_time_server, :warnings)
+-- #{ table.duration
+-- # { insert
+-- #  :xuid string
+-- #  :voted string
+-- #  :donated string
+-- #  :muted string
+-- #  :lastplayed string
+-- #  :totalonline string
+-- #  :time_join_server
+-- #  :warnings int
+INSERT INTO duration(xuid, voted, donated, muted, lastplayed, totalonline, warnings) VALUES (:xuid, :voted, :donated, :muted, :lastplayed, :totalonline, :time_join_server, :warnings)
 -- #}
 ------------------------------------------
 
@@ -24,7 +33,15 @@ INSERT INTO duration(xuid, voted, donated, muted, lastplayed, totalonline, warni
 CREATE TABLE IF NOT EXISTS murders(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, combo INT, gapple INT, nodebuff INT, trapping INT, bridge INT, classic INT)
 -- #}
 
--- #{ table.murders.insert
+-- #{ table.murders
+-- # { insert
+-- #  :xuid string
+-- #  :combo int
+-- #  :gapple int
+-- #  :nodebuff int
+-- #  :trapping int
+-- #  :bridge int
+-- #  :classic int
 INSERT INTO murders(xuid, combo, gapple, nodebuff, trapping, bridge, classic) VALUES (:xuid, :combo, :gapple, :nodebuff, :trapping, :bridge, :classic)
 -- #}
 
@@ -32,7 +49,15 @@ INSERT INTO murders(xuid, combo, gapple, nodebuff, trapping, bridge, classic) VA
 CREATE TABLE IF NOT EXISTS kills(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, combo INT, gapple INT, nodebuff INT, trapping INT, bridge INT, classic INT)
 -- #}
 
--- #{ table.kills.insert
+-- #{ table.kills
+-- # { insert
+-- #   :xuid string
+-- #   :combo int
+-- #   :gapple int
+-- #   :nodebuff int
+-- #   :trapping int
+-- #   :bridge int
+-- #   :classic int
 INSERT INTO kills(xuid, combo, gapple, nodebuff, trapping, bridge, classic) VALUES (:xuid, :combo, :gapple, :nodebuff, :trapping, :bridge, :classic)
 -- #}
 
@@ -44,8 +69,16 @@ INSERT INTO kills(xuid, combo, gapple, nodebuff, trapping, bridge, classic) VALU
 CREATE TABLE IF NOT EXISTS points(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, combo INT, gapple INT, nodebuff INT, trapping INT, bridge INT, classic INT)
 -- #}
 
--- #{ table.points.insert
-INSERT INTO points(xuid, name, combo, gapple, nodebuff, trapping, bridge, classic) VALUES (:xuid, :combo, :gapple, :nodebuff, :trapping, :bridge, :classic)
+-- #{ points
+-- #  { insert
+-- #  :xuid string
+-- #  :combo int
+-- #  :gapple int
+-- #  :nodebuff int
+-- #  :trapping int
+-- #  :bridge int
+-- #  :classic int
+INSERT INTO points(xuid, combo, gapple, nodebuff, trapping, bridge, classic) VALUES (:xuid, :combo, :gapple, :nodebuff, :trapping, :bridge, :classic)
 -- #}
 
 ------------------------------------------
@@ -61,11 +94,17 @@ CREATE TABLE IF NOT EXISTS staff_stats(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY 
 --WON EVENTS (Yes, it will have records of all the events you won, damn pro....)
 
 -- #{ table.won_events
-CREATE TABLE IF NOT EXISTS won_events(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, username VARCHAR(30) NOT NULL, title TEXT, description VARCHAR(100), prize VARCHAR(30))
+CREATE TABLE IF NOT EXISTS won_events(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, name VARCHAR(30) NOT NULL, title TEXT, description VARCHAR(100), prize VARCHAR(30))
 -- #}
 
--- #{ table.won_events.insert
-INSERT INTO won_events(xuid, username, title, description, prize) VALUES (:xuid, :username, :title, :description, :prize)
+-- #{ won_events
+-- #  { insert
+-- #  :xuid string
+-- #  :name string
+-- #  :title string
+-- #  :description string
+-- #  :prize string
+INSERT INTO won_events(xuid, name, title, description, prize) VALUES (:xuid, :username, :title, :description, :prize)
 -- #}
 
 ------------------------------------------
@@ -76,14 +115,20 @@ CREATE TABLE IF NOT EXISTS kits(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, us
 -- #}
 
 ------------------------------------------
-
 --user data, obviously
 
 -- #{ table.user_data
 CREATE TABLE IF NOT EXISTS user_data(xuid VARCHAR(18) NOT NULL UNIQUE, name VARCHAR(30) NOT NULL, custom_name VARCHAR(30) NULL, alias VARCHAR(25) NULL, language TEXT, skin LONGTEXT, coin INT)
 -- #}
 
--- #{ table.user_data.insert
+-- #{ user_data
+-- #  { insert
+-- # :xuid string
+-- # :name string
+-- # :custom_name string
+-- # :alias string
+-- # :language: string
+-- # :skin string
 INSERT INTO user_data(xuid, name, custom_name, alias, language, skin, coin) VALUES (:xuid, :name, :custom_name, :alias, :language, :skin, :coin)
 -- #}
 
@@ -95,8 +140,14 @@ INSERT INTO user_data(xuid, name, custom_name, alias, language, skin, coin) VALU
 CREATE TABLE IF NOT EXISTS settings(xuid VARCHAR(18) NOT NULL UNIQUE, scoreboard BOOLEAN, queue BOOLEAN, cps BOOLEAN, auto_join BOOLEAN)
 -- #}
 
--- #{ table.settings.insert
-INSERT INTO settings(xuid, name, scoreboard, queue, cps, auto_jon) VALUES (:xuid, :scoreboard, :cps, :auto_join)
+-- #{ settings
+-- #  { insert
+-- #  :xuid string
+-- #  :scoreboard bool
+-- #  :queue bool
+-- #  :cps bool
+-- #  :auto_join bool
+INSERT INTO settings(xuid, scoreboard, queue, cps, auto_jon) VALUES (:xuid, :scoreboard, :cps, :auto_join)
 -- #}
 
 ------------------------------------------
