e2e269d5633eba159d58b9cd6d57cf1e29887916
srclauyt@gmail.com <srclauyt@gmail.com>
2023-12-25 23:28:41 -0300

    fucking mistake, i hate you

--- resources/mysql.sql
+++ resources/mysql.sql
@@ -1,13 +1,5 @@
 -- #! mysql
 
--- #{ table.bans
-CREATE TABLE IF NOT EXISTS bans(id INT AUTO_INCREMENT PRIMARY KEY, banned_user VARCHAR(30) NOT NULL, event VARCHAR(25), expired TEXT, reason TEXT, staff VARCHAR(30))
--- #}
-
--- #{ query.declarartion
-CREATE TABLE IF NOT EXISTS duration(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, voted TEXT, donated TEXT, muted TEXT, lastplayed TEXT, totalonline TEXT, time_join_server TEXT, warnings INT)
--- #}
-
 -- #{ duration
 -- # { insert
 -- #  :xuid string
@@ -20,10 +12,6 @@ CREATE TABLE IF NOT EXISTS duration(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY
 -- #  :warnings int
 INSERT INTO duration(xuid, voted, donated, muted, lastplayed, totalonline, warnings) VALUES (:xuid, :voted, :donated, :muted, :lastplayed, :totalonline, :time_join_server, :warnings)
 -- #}
--- #}
-
--- #{ query.declarartion
-CREATE TABLE IF NOT EXISTS murders(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, combo INT, gapple INT, nodebuff INT, trapping INT, bridge INT, classic INT)
 -- #}
 
 -- #{ murders
@@ -39,10 +27,6 @@ INSERT INTO murders(xuid, combo, gapple, nodebuff, trapping, bridge, classic) VA
 -- #}
 -- #}
 
--- #{ query.declarartion
-CREATE TABLE IF NOT EXISTS kills(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, combo INT, gapple INT, nodebuff INT, trapping INT, bridge INT, classic INT)
--- #}
-
 -- #{ kills
 -- # { insert
 -- #   :xuid string
@@ -56,10 +40,6 @@ INSERT INTO kills(xuid, combo, gapple, nodebuff, trapping, bridge, classic) VALU
 -- #}
 -- #}
 
--- #{ query.declarartion
-CREATE TABLE IF NOT EXISTS points(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, combo INT, gapple INT, nodebuff INT, trapping INT, bridge INT, classic INT)
--- #}
-
 -- #{ points
 -- #  { insert
 -- #  :xuid string
@@ -71,14 +51,6 @@ CREATE TABLE IF NOT EXISTS points(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, 
 -- #  :classic int
 INSERT INTO points(xuid, combo, gapple, nodebuff, trapping, bridge, classic) VALUES (:xuid, :combo, :gapple, :nodebuff, :trapping, :bridge, :classic)
 -- #}
--- #}
-
--- #{ query.declarartion
-CREATE TABLE IF NOT EXISTS staff_stats(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, name VARCHAR(30) NOT NULL, kicks INT, silenceds INT, reports INT)
--- #}
-
--- #{ query.declarartion
-CREATE TABLE IF NOT EXISTS won_events(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, name VARCHAR(30) NOT NULL, title TEXT, description VARCHAR(100), prize VARCHAR(30))
 -- #}
 
 -- #{ won_events
@@ -96,10 +68,6 @@ INSERT INTO won_events(xuid, name, title, description, prize) VALUES (:xuid, :us
 CREATE TABLE IF NOT EXISTS kits(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, username VARCHAR(30) NOT NULL)
 -- #}
 
--- #{ query.declarartion
-CREATE TABLE IF NOT EXISTS user_data(xuid VARCHAR(18) NOT NULL UNIQUE, name VARCHAR(30) NOT NULL, custom_name VARCHAR(30) NULL, alias VARCHAR(25) NULL, language TEXT, skin LONGTEXT, coin INT)
--- #}
-
 -- #{ user_data
 -- #  { insert
 -- # :xuid string
@@ -112,10 +80,6 @@ INSERT INTO user_data(xuid, name, custom_name, alias, language, skin, coin) VALU
 -- #}
 -- #}
 
--- #{ query.declarartion
-CREATE TABLE IF NOT EXISTS settings(xuid VARCHAR(18) NOT NULL UNIQUE, scoreboard BOOLEAN, queue BOOLEAN, cps BOOLEAN, auto_join BOOLEAN)
--- #}
-
 -- #{ settings
 -- #  { insert
 -- #  :xuid string
@@ -125,8 +89,4 @@ CREATE TABLE IF NOT EXISTS settings(xuid VARCHAR(18) NOT NULL UNIQUE, scoreboard
 -- #  :auto_join bool
 INSERT INTO settings(xuid, scoreboard, queue, cps, auto_jon) VALUES (:xuid, :scoreboard, :cps, :auto_join)
 -- #}
--- #}
-
--- #{ alter.table.bans
-ALTER TABLE bans AUTO_INCREMENT = 0
 -- #}
--- src/isrdxv/practice/Loader.php
+++ src/isrdxv/practice/Loader.php
@@ -88,13 +88,25 @@ class Loader extends PluginBase
             "mysql" => "mysql.sql"
     ]);
     $logger = $this->getLogger();
-    $this->database->executeGeneric("table.bans", [], function() use($logger): void {
+    $this->database->executeGeneric("CREATE TABLE IF NOT EXISTS kills(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, combo INT, gapple INT, nodebuff INT, trapping INT, bridge INT, classic INT)", [], function() use($logger): void {
+      $logger->warning("MySQL Table: stats, loaded correctly");
+    }, function(SqlError $error_) use(&$error){
+      $error = $error_;
+    });
+    $this->database->waitAll();
+        $this->database->executeGeneric("CREATE TABLE IF NOT EXISTS murders(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, combo INT, gapple INT, nodebuff INT, trapping INT, bridge INT, classic INT)", [], function() use($logger): void {
+      $logger->warning("MySQL Table: stats, loaded correctly");
+    }, function(SqlError $error_) use(&$error){
+      $error = $error_;
+    });
+    $this->database->waitAll();
+    $this->database->executeGeneric("CREATE TABLE IF NOT EXISTS bans(id INT AUTO_INCREMENT PRIMARY KEY, banned_user VARCHAR(30) NOT NULL, event VARCHAR(25), expired TEXT, reason TEXT, staff VARCHAR(30));", [], function() use($logger): void {
       $logger->warning("MySQL Table: bans, loaded correctly");
     }, function(SqlError $error_) use(&$error){
       $error = $error_;
     });
     $this->database->waitAll();
-    $this->database->executeGeneric("table.duration", [], function() use($logger): void {
+    $this->database->executeGeneric("CREATE TABLE IF NOT EXISTS duration(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, voted TEXT, donated TEXT, muted TEXT, lastplayed TEXT, totalonline TEXT, time_join_server TEXT, warnings INT);", [], function() use($logger): void {
       $logger->warning("MySQL Table: duration, loaded correctly");
     }, function(SqlError $error_) use(&$error){
       $error = $error_;
@@ -106,25 +118,37 @@ class Loader extends PluginBase
       $error = $error_;
     });
     $this->database->waitAll();
-    $this->database->executeGeneric("table.staff.stats", [], function() use($logger): void {
+    $this->database->executeGeneric("CREATE TABLE IF NOT EXISTS settings(xuid VARCHAR(18) NOT NULL UNIQUE, scoreboard BOOLEAN, queue BOOLEAN, cps BOOLEAN, auto_join BOOLEAN)", [], function() use($logger): void {
+      $logger->warning("MySQL Table: stats, loaded correctly");
+    }, function(SqlError $error_) use(&$error){
+      $error = $error_;
+    });
+    $this->database->waitAll();
+    $this->database->executeGeneric("CREATE TABLE IF NOT EXISTS staff_stats(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, name VARCHAR(30) NOT NULL, kicks INT, silenceds INT, reports INT);", [], function() use($logger): void {
       $logger->warning("MySQL Table: staff.stats, loaded correctly");
     }, function(SqlError $error_) use(&$error){
       $error = $error_;
     });
     $this->database->waitAll();
-    $this->database->executeGeneric("table.kits", [], function() use($logger): void {
-      $logger->warning("MySQL Table: kits, loaded correctly");
+    $this->database->executeGeneric("CREATE TABLE IF NOT EXISTS won_events(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, name VARCHAR(30) NOT NULL, title TEXT, description VARCHAR(100), prize VARCHAR(30))", [], function() use($logger): void {
+      $logger->warning("MySQL Table: stats, loaded correctly");
+    }, function(SqlError $error_) use(&$error){
+      $error = $error_;
+    });
+    $this->database->waitAll();    
+    $this->database->executeGeneric("CREATE TABLE IF NOT EXISTS points(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, combo INT, gapple INT, nodebuff INT, trapping INT, bridge INT, classic INT);", [], function() use($logger): void {
+      $logger->warning("MySQL Table: points, loaded correctly");
     }, function(SqlError $error_) use(&$error){
       $error = $error_;
     });
     $this->database->waitAll();
-    $this->database->executeGeneric("table.user_data", [], function() use($logger): void {
+    $this->database->executeGeneric("CREATE TABLE IF NOT EXISTS user_data(xuid VARCHAR(18) NOT NULL UNIQUE, name VARCHAR(30) NOT NULL, custom_name VARCHAR(30) NULL, alias VARCHAR(25) NULL, language TEXT, skin LONGTEXT, coin INT);", [], function() use($logger): void {
       $logger->warning("MySQL Table: user_data, loaded correctly");
     }, function(SqlError $error_) use(&$error){
       $error = $error_;
     });
     $this->database->waitAll();
-    $this->database->executeGeneric("alter.table.bans", [], function() use($logger): void {
+    $this->database->executeGeneric("ALTER TABLE bans AUTO_INCREMENT = 0", [], function() use($logger): void {
       $logger->warning("MySQL Table: bans, alter correctly");
     }, function(SqlError $error_) use(&$error){
       $error = $error_;
