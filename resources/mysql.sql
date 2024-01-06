6a3784a66f14084455e15e16cd82f5f417a84ffd
srclauyt@gmail.com <srclauyt@gmail.com>
2024-01-06 18:21:51 -0300

    update mysql.sql

--- resources/mysql.sql
+++ resources/mysql.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS bans(id INT AUTO_INCREMENT PRIMARY KEY, banned_user V
 -- # }
 
 -- # { init.duration
-CREATE TABLE IF NOT EXISTS duration(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, warnings INT, voted TEXT, donated TEXT, muted TEXT, lastplayed TEXT, totalonline TEXT, time_join_server TEXT);
+CREATE TABLE IF NOT EXISTS duration(xuid VARCHAR(18) NOT NULL UNIQUE PRIMARY KEY, time_join_server TEXT, warnings INT, voted TEXT, donated TEXT, muted TEXT, lastplayed TEXT, totalonline TEXT);
 -- # }
 
 -- # { init.staff.stats
@@ -50,7 +50,7 @@ ALTER TABLE bans AUTO_INCREMENT = 0;
 -- #  :totalonline string
 -- #  :warnings int
 -- #  :time_join_server string
-INSERT OR REPLACE INTO duration(xuid, warnings, voted, donated, muted, lastplayed, totalonline, time_join_server) VALUES (:xuid, :warnings, :voted, :donated, :muted, :lastplayed, :totalonline, :time_join_server)
+INSERT OR REPLACE INTO duration(xuid, time_join_server, warnings, voted, donated, muted, lastplayed, totalonline) VALUES (:xuid, :time_join_server, :warnings, :voted, :donated, :muted, :lastplayed, :totalonline)
 -- # }
 
 -- # { murders
