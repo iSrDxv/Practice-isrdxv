4da1d148f242b704ea45f8545cbddef7a4504dba
srclauyt@gmail.com <srclauyt@gmail.com>
2024-01-10 16:45:53 -0300

    update LobbyScoreboard.php

--- src/libs/scoreboard/type/LobbyScoreboard.php
+++ src/libs/scoreboard/type/LobbyScoreboard.php
@@ -30,11 +30,15 @@ class LobbyScoreboard extends Scoreboard
   {
     parent::setLine(1, "");
     parent::setLine(2, TextFormat::colorize(" &bOnline: &f" . count(Server::getInstance()->getOnlinePlayers())));
-    parent::setLine(3, TextFormat::colorize(" &bPing: &f" . parent::getPlayer()->getNetworkSession()->getPing()));
-    parent::setLine(4, TextFormat::colorize(" &bIn Game: &f" . Loader::getInstance()->getGameManager()->getGameCount()));
-    parent::setLine(5, TextFormat::colorize(" &bQueued: &f" . Loader::getInstance()->getQueueManager()->getQueueCount()));
-    parent::setLine(6, TextFormat::colorize(" &o&7" . Loader::getInstance()->getConfig()->getNested("server-address")));
-    parent::setLine(7, TextFormat::colorize("§f"));
+    parent::setLine(3, str_repeat(" ", 20));
+    parent::setLine(4, TextFormat::colorize(" &bPing: &f" . parent::getPlayer()->getNetworkSession()->getPing()));
+    parent::setLine(5, str_repeat(" ", 5));
+    parent::setLine(6, TextFormat::colorize(" &bIn Game: &f" . Loader::getInstance()->getGameManager()->getGameCount()));
+    parent::setLine(7, str_repeat(" ", 4));
+    parent::setLine(8, TextFormat::colorize(" &bQueued: &f" . Loader::getInstance()->getQueueManager()->getQueueCount()));
+    parent::setLine(9, str_repeat(" ", 3));
+    parent::setLine(10, TextFormat::colorize(" &o&7" . Loader::getInstance()->getConfig()->getNested("server-address")));
+    parent::setLine(11, TextFormat::colorize("§f"));
   }
   
 }
